99cf71c4cbace5e2a0dc853fdcb5a0dc53d7a50a
eyyu <eva.yw.yu@gmail.com>
2016-10-13 21:42:50 -0700

    EY: Proof of Concept 1 -- simple barcode scanner with zxing and zxing android embedded libs

---
+++ BarcodeScanner/app/src/main/java/com/comp3711/eva/barcodescanner/MainActivity.java
@@ -0,0 +1,51 @@
+package com.comp3711.eva.barcodescanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Activity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        activity = this;
+    }
+
+    public void buttonClickQRGen(View v)
+    {
+        IntentIntegrator integrator = new IntentIntegrator(activity);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setBeepEnabled(true);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result.getContents() == null)
+        {
+            Log.d("MainActivity", "cancelled scan");
+            Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
