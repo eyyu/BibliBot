cc44ad2e90da3b1db6cad8d104e53e6c326a2d89
Robert Arendac <robertarendac@gmail.com>
2016-10-20 11:06:05 -0700

    The citation select wireframe

---
+++ Biblibot/app/src/main/java/com/example/bobo/biblibot/CitationActivity.java
@@ -0,0 +1,22 @@
+package com.example.bobo.biblibot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CitationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_citation);
+    }
+
+    public void scan(final View view) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void generate(final View view) {
+        throw new UnsupportedOperationException();
+    }
+}
