247c72ba3f891d0d44b56bca56bd096c2d3577ec
Robert Arendac <robertarendac@gmail.com>
2016-11-18 14:51:48 -0800

    Adding citation class

---
+++ Biblibot-merged/app/src/main/java/com/comp3711/eva/biblibot/Citation.java
@@ -0,0 +1,188 @@
+package com.comp3711.eva.biblibot;
+
+/**
+ * Created by Bobo on 18/11/2016.
+ */
+
+public class Citation {
+    private String   fName;
+    private String   lName;
+    private char     initial;
+    private String   title;
+    private String   publisher;
+    private String   pudDate;
+    private String   volume;
+    private String   issue;
+    private String   container;
+    private String   version;
+    private String   location;
+    private String[] contributors;
+    private String   accessDate;
+    private String   subtitle;
+    private String   artTitle;
+    private String   perTitle;
+    private String   url;
+    private String   pages;
+    private String   pubYear;
+    private String   pubDateOrAccessDate;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public char getInitial() {
+        return initial;
+    }
+
+    public void setInitial(char initial) {
+        this.initial = initial;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPudDate() {
+        return pudDate;
+    }
+
+    public void setPudDate(String pudDate) {
+        this.pudDate = pudDate;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public String getContainer() {
+        return container;
+    }
+
+    public void setContainer(String container) {
+        this.container = container;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String[] getContributors() {
+        return contributors;
+    }
+
+    public void setContributors(String[] contributors) {
+        this.contributors = contributors;
+    }
+
+    public String getAccessDate() {
+        return accessDate;
+    }
+
+    public void setAccessDate(String accessDate) {
+        this.accessDate = accessDate;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getArtTitle() {
+        return artTitle;
+    }
+
+    public void setArtTitle(String artTitle) {
+        this.artTitle = artTitle;
+    }
+
+    public String getPerTitle() {
+        return perTitle;
+    }
+
+    public void setPerTitle(String perTitle) {
+        this.perTitle = perTitle;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getPubYear() {
+        return pubYear;
+    }
+
+    public void setPubYear(String pubYear) {
+        this.pubYear = pubYear;
+    }
+
+    public String getPubDateOrAccessDate() {
+        return pubDateOrAccessDate;
+    }
+
+    public void setPubDateOrAccessDate(String pubDateOrAccessDate) {
+        this.pubDateOrAccessDate = pubDateOrAccessDate;
+    }
+}
